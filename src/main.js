edc1eb8ad656008f65bac0a2ad691cefece0b632
Fulling <2435406817@qq.com>
2022-04-06 17:05:45 +0800

    vue初始化

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
+// 请求基准路径
+axios.defaults.baseURL = "http://127.0.0.1:5000"
+
+Vue.prototype.$axios = axios
+Vue.prototype.$echarts = window.echarts
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  mounted(){
+    console.log(this);
+  }
 }).$mount('#app')
