2d7e814df1005b7af66c3c055133220349dd28c0
Fulling <2435406817@qq.com>
2022-04-06 15:20:38 +0800

    ok

--- KOA_SERVER/middleware/koa_response_data.js
+++ KOA_SERVER/middleware/koa_response_data.js
@@ -1,15 +1,23 @@
 const path = require("path");
-const fileUtils = require("../utils/file_utils")
+const fileUtils = require("../utils/file_utils");
 
 module.exports = async (context, next) => {
-    // 根据url访问指定文件
-    const url = context.request.url
-    let filePath = "../data" + url + ".json"
-    filePath = path.join(__dirname, filePath)
+  // 根据url访问指定文件
+  const url = context.request.url;
+  let filePath = "../data" + url + ".json";
+  filePath = path.join(__dirname, filePath);
 
+  try {
     // 访问文件内容
-    const ret = fileUtils.getFileJsonData(filePath)
-    context.response.body = ret
-    
-    await next()
+    const ret = await fileUtils.getFileJsonData(filePath);
+    context.response.body = ret;
+  } catch (error) {
+    const errMsg = {
+      msg: "文件不存在",
+      status: 404,
+    };
+    context.response.body = JSON.stringify(errMsg);
+  }
+
+  await next();
 };
--- KOA_SERVER/utils/file_utils.js
+++ KOA_SERVER/utils/file_utils.js
@@ -1,5 +1,14 @@
-const fs = require("fs")
-module.exports.getFileJsonData = (filePath)=>{
-    // 根据文件路径读取文件内容
-    // fs.readFile
-} 
+const fs = require("fs");
+module.exports.getFileJsonData = (filePath) => {
+  // 根据文件路径读取文件内容
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf-8", (error, data) => {
+      if (error) {
+        // 读取文件失败
+        reject(error);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
