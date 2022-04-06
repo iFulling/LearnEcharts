6d2e9692eb7b64e76ed2d2a8f2c2e563d34022a7
Fulling <2435406817@qq.com>
2022-04-06 15:35:52 +0800

    允许跨域

--- KOA_SERVER/middleware/koa_response_header.js
+++ KOA_SERVER/middleware/koa_response_header.js
@@ -1,5 +1,8 @@
-module.exports = async (context, next)=>{
-    const contentType = 'application/json; charset=utf-8'
-    context.set("content-Type", contentType)
-    await next()
-}
+module.exports = async (context, next) => {
+  const contentType = "application/json; charset=utf-8";
+  context.set("content-Type", contentType);
+  // 允许跨域
+  context.set("access-control-allow-origin", "*");
+  context.set("access-control-allow-methods", "options,get,put,post,delete");
+  await next();
+};
