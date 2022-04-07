a3d0f6d4532a553326712758a9ff9e2ceb8281f9
Fulling <2435406817@qq.com>
2022-04-07 22:27:53 +0800

    rank

--- src/components/Rank.vue
+++ src/components/Rank.vue
@@ -11,6 +11,9 @@ export default {
     return {
       echartsInstance: null,
       allData: null,
+      startValue: 0,
+      endValue: 9,
+      timer: null,
     };
   },
   methods: {
@@ -29,6 +32,9 @@ export default {
           bottom: "5%",
           containLabel: true,
         },
+        tooltip: {
+          show: true,
+        },
         xAxis: {
           type: "category",
         },
@@ -42,7 +48,12 @@ export default {
         ],
       };
       this.echartsInstance.setOption(initOption);
-      this.screenAdapter();
+      this.echartsInstance.on("mouseover", () => {
+        clearInterval(this.timer);
+      });
+      this.echartsInstance.on("mouseout", () => {
+        this.startInterval();
+      });
     },
     async getData() {
       const { data } = await this.$axios.get("rank");
@@ -52,8 +63,14 @@ export default {
       });
       console.log(data);
       this.updataChart();
+      this.startInterval();
     },
     updataChart() {
+      const colorArr = [
+        ["#0BA82C", "#4FF778"],
+        ["#2E72BF", "#23E5E5"],
+        ["#5052EE", "#Ab6EE5"],
+      ];
       const provinceArr = this.allData.map((item) => {
         return item.name;
       });
@@ -64,18 +81,74 @@ export default {
         xAxis: {
           data: provinceArr,
         },
+        dataZoom: {
+          show: false,
+          startValue: this.startValue,
+          endValue: this.endValue,
+        },
         series: [
           {
             data: valueArr,
+            itemStyle: {
+              color: (arg) => {
+                let targetColorArr = null;
+                if (arg.value > 300) {
+                  targetColorArr = colorArr[0];
+                } else if (arg.value > 200) {
+                  targetColorArr = colorArr[1];
+                } else {
+                  targetColorArr = colorArr[2];
+                }
+                return new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  {
+                    offset: 0,
+                    color: targetColorArr[0],
+                  },
+                  {
+                    offset: 1,
+                    color: targetColorArr[1],
+                  },
+                ]);
+              },
+            },
           },
         ],
       };
       this.echartsInstance.setOption(dataOption);
     },
     screenAdapter() {
-      const adapterOption = {};
+      const titleFontSize = (this.$refs.rank_ref.offsetWidth / 100) * 3.6;
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: titleFontSize,
+          },
+        },
+        series: [
+          {
+            barWidth: titleFontSize,
+            itemStyle: {
+              barBorderRadius: [titleFontSize / 2, titleFontSize / 2, 0, 0],
+            },
+          },
+        ],
+      };
       this.echartsInstance.setOption(adapterOption);
       this.echartsInstance.resize();
+    },
+    startInterval() {
+      if (this.timer) {
+        clearInterval(this.timer);
+      }
+      this.timer = setInterval(() => {
+        this.startValue++;
+        this.endValue++;
+        if (this.endValue > this.allData.length - 1) {
+          this.startValue = 0;
+          this.endValue = 9;
+        }
+        this.updataChart();
+      }, 2000);
     },
   },
   mounted() {
@@ -86,6 +159,7 @@ export default {
   },
   beforeDestroy() {
     window.removeEventListener("resize", this.screenAdapter);
+    clearInterval(this.timer);
   },
 };
 </script>
